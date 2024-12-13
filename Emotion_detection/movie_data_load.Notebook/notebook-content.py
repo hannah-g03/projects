cefe0c2cb557b4803ab159ec1134b5a271ea8dbd
hannah-g03 <112111591+hannah-g03@users.noreply.github.com>
2024-12-13 13:37:24 +0000

    Update notebook-content.py

--- Emotion_detection/movie_data_load.Notebook/notebook-content.py
+++ Emotion_detection/movie_data_load.Notebook/notebook-content.py
@@ -1,106 +1,25 @@
 # Fabric notebook source
-
-# METADATA ********************
-
-# META {
-# META   "kernel_info": {
-# META     "name": "synapse_pyspark"
-# META   },
-# META   "dependencies": {
-# META     "lakehouse": {
-# META       "default_lakehouse": "7028769d-5193-4c93-974a-a1bd0a93905e",
-# META       "default_lakehouse_name": "MovieData",
-# META       "default_lakehouse_workspace_id": "346e1b64-7681-4e36-aaf6-454ec69177f7"
-# META     }
-# META   }
-# META }
-
-# CELL ********************
-
 import os
 import json
 from delta import *
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 movie_df = spark.read.format("csv").option("header","true").load("Files/movie.csv")
 display(movie_df)
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
 
 rating_df = spark.read.format("csv").option("header","true").load("Files/rating.csv")
 display(rating_df)
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 tag_df = spark.read.format("csv").option("header","true").load("Files/tag.csv")
 display(tag_df)
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
 
 link_df = spark.read.format("csv").option("header","true").load("Files/link.csv")
 display(link_df)
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 genome_df = spark.read.format("csv").option("header","true").load("Files/genome_tags.csv")
 display(genome_df)
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 genome_scores_df = spark.read.format("csv").option("header","true").load("Files/genome_scores.csv")
 display(genome_scores_df)
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
 
 movie_df.write.format("delta").option("mergeSchema", "true") \
     .mode("overwrite").saveAsTable("movie_list")
@@ -120,9 +39,3 @@ genome_df.write.format("delta").option("mergeSchema", "true") \
 genome_scores_df.write.format("delta").option("mergeSchema", "true") \
     .mode("overwrite").saveAsTable("genome_relevance_scores")
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
