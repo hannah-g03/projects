942311cfc8a9154a6ee96b7e156cf96ca360f896
hannah-g03 <112111591+hannah-g03@users.noreply.github.com>
2024-12-13 13:48:29 +0000

    Update notebook-content.py

--- Weather_project/Gold &#124; date_dimension.Notebook/notebook-content.py
+++ Weather_project/Gold &#124; date_dimension.Notebook/notebook-content.py
@@ -1,22 +1,5 @@
 # Fabric notebook source
 
-# METADATA ********************
-
-# META {
-# META   "kernel_info": {
-# META     "name": "synapse_pyspark"
-# META   },
-# META   "dependencies": {
-# META     "lakehouse": {
-# META       "default_lakehouse": "a40bc079-4900-4630-97c0-902c8c1e2328",
-# META       "default_lakehouse_name": "Weather_lakehouse",
-# META       "default_lakehouse_workspace_id": "346e1b64-7681-4e36-aaf6-454ec69177f7"
-# META     }
-# META   }
-# META }
-
-# CELL ********************
-
 import os
 import json
 from pyspark.sql import SparkSession
@@ -24,18 +7,8 @@ from pyspark.sql.functions import col, date_format
 from pyspark.sql.types import *
 import datetime as dt
 
-# METADATA ********************
 
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# MARKDOWN ********************
-
-# ##### **Creating dimension table for date**
-
-# CELL ********************
+###### **Creating dimension table for date**
 
 # Define the date range
 start_date = dt.date(2024, 7, 24)
@@ -52,21 +25,6 @@ date_df = date_df.withColumn("year", date_format(col("date"), "yyyy").cast("int"
     .withColumn("day", date_format(col("date"), "dd").cast("int")) \
     .withColumn("day_of_week", date_format(col("date"), "E")) 
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 # save as delta table
 date_df.write.format("delta").mode("overwrite").saveAsTable("dim_date")
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
